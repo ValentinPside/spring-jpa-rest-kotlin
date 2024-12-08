d12c3d5d35a66b5c11008f7756a7d1808526d2c5
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2024-12-08 17:47:47 +0300

    Request with sort

--- src/main/kotlin/com/valmal/repository/DepartmentsRepository.kt
+++ src/main/kotlin/com/valmal/repository/DepartmentsRepository.kt
@@ -4,6 +4,5 @@ import com.valmal.entity.DepartmentEntity
 import org.springframework.data.repository.CrudRepository
 
 interface DepartmentsRepository: CrudRepository<DepartmentEntity, Int>{
-
     fun findByOrderByName(): List<DepartmentEntity>
 }
